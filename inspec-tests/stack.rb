0cde45005bbd44ef7904351e8758bcc29c7c4188
vagrant <vagrant@precise64.(none)>
2018-09-12 07:10:58 +0000

    stack.rb

---
+++ inspec-tests/stack.rb
@@ -0,0 +1,19 @@
+describe service ('tomcat7') do
+it {should be_installed }
+it { should be_enabled }
+it { should be_running }
+end
+
+describe service ('mysql') do
+it {should be_installed }
+it { should be_enabled }
+it { should be_running}
+end
+
+#describe http('http://10.1.1.34:8080/MyAppDemo') do 
+#its('status') {should cmp 200}
+#end
+
+describe port(8080) do
+it { should be_listening}
+end
